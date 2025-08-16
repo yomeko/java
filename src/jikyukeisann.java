d794158a5b28efd1198be3f5d1f5d6ae229f5bfd
yuyukiti <mintoayumu1005@icloud.com>
2025-08-16 17:14:18 +0900

    あああ

--- src/jikyukeisann.java
+++ src/jikyukeisann.java
@@ -1,9 +1,8 @@
 
-public class main {
+public class jikyukeisann {
 	public static void mian(String[]args) {
 		int jikyu;
-		jikyu = 979;
+		jikyu = 979;//時給
 		System.out.println(jikyu);
-		
 	}
 }
